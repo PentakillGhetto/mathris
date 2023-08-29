2c2cfd0c03d2c7c31fffb6d907699b1ce3924082
Gleb <titovv-glebb@yandex.ru>
2023-08-29 11:16:58 +0300

    Add VSCode changes

---
+++ Assets/!/Scripts/Tets.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+[ExecuteInEditMode]
+public class Tets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("Hello");
+    }
+}
